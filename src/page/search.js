6ef3b6106ebd7b1b4929d7a2d7ac4d9e438bd7a2
berabjesus <64360974+Berabjesus@users.noreply.github.com>
2020-12-19 00:24:17 +0300

    ADD border and shadow properties to search box items

--- src/page/search.js
+++ src/page/search.js
@@ -3,14 +3,14 @@ import {it} from '../module/main_module'
 
 const section = () => {
   const container = it.is('section')
-  container.classes('d-flex flex-column w-100 ss-search-container custom-border')
+  container.classes('d-flex flex-column w-100 ss-search-container')
   const header = it.is('h2')
   header.innerText = '< aurApp />'
 
   const inputContainer = it.is('div')
-  inputContainer.classes('row d-flex flex-column align-items-center px-5 mt-1 pb-0 mb-0 h-100 position-relative custom-border')
+  inputContainer.classes('row d-flex flex-column align-items-center px-2 pt-1 mt-1 pb-0 mb-0 h-100 position-relative')
   const formGroup = it.is('div')
-  formGroup.classes('form-group col-12 col-md-10 col-lg-8')
+  formGroup.classes('form-group col-12 col-md-10 col-lg-7')
   const input = it.is('input')
   input.classes('form-control px-3 ss-input-form')
   input.placeholder = 'Enter city or country name here'
