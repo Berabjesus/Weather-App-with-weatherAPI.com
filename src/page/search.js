09eb32561e3ab909e0780856e4b86b66ab52c213
berabjesus <64360974+Berabjesus@users.noreply.github.com>
2020-12-18 23:51:46 +0300

    ADD flex 0 property to input fie;d

--- src/page/search.js
+++ src/page/search.js
@@ -8,20 +8,21 @@ const section = () => {
   header.innerText = '< aurApp />'
 
   const inputContainer = it.is('div')
-  inputContainer.classes('row d-flex flex-column align-items-center form-group px-5 mt-1')
+  inputContainer.classes('row d-flex flex-column align-items-center form-group px-5 mt-1 pb-0 mb-0 h-100 position-relative custom-border')
   const input = it.is('input')
-  input.classes('form-control col-12 col-md-10 col-lg-8 px-3 ss-input-form')
+  input.classes('form-control col-12 col-md-10 col-lg-8 flex-0 px-3 ss-input-form')
   input.placeholder = 'Enter city or country name here'
   input.setAttribute('list', 'cityName')
   const datalist = it.is('datalist')
   datalist.id = "cityName";
-  inputContainer.append(input, datalist)
-
   const searchButton = it.is('button')
   searchButton.classes('ss-custom-btn')
   searchButton.innerText = 'Search'
 
-  container.append(header, inputContainer, searchButton)
+  inputContainer.append(input, datalist, searchButton)
+
+
+  container.append(header, inputContainer)
   return container
 }
 
