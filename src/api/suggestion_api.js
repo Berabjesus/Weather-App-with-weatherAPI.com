380cdf4493432fd1bc59d8efd2bd81eb0a4f6f5c
berabjesus <64360974+Berabjesus@users.noreply.github.com>
2020-12-20 15:21:52 +0300

    UPDATE https of the suggestion api

--- src/api/suggestion_api.js
+++ src/api/suggestion_api.js
@@ -2,7 +2,7 @@ const API_KEY = process.env.WEATHER_API_KEY;
 
 const getSuggestions = async value => {
   try {
-    const response = await fetch(`http://api.weatherapi.com/v1/search.json?key=${API_KEY}&q=${value}`);
+    const response = await fetch(`https://api.weatherapi.com/v1/search.json?key=${API_KEY}&q=${value}`);
     if (!response.ok) {
       return response;
     }
